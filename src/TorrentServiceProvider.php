15e01d120596521205c0c57bee1a6e04f226d8ce
Bastian Stien <bastian.stien@gmail.com>
2015-03-02 18:32:57 +0100

    Initial commit

---
+++ src/TorrentServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace Stien\Torrent;
+
+use Illuminate\Support\ServiceProvider;
+
+class TorrentServiceProvider extends ServiceProvider {
+
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+}
