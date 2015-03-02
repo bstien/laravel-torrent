91535447e0641698d30fc0c7dd24f27c4aa65e51
Bastian Stien <bastian.stien@gmail.com>
2015-03-02 22:15:21 +0100

    Updated PirateBayAdapter

--- src/Adapter/PirateBayAdapter.php
+++ src/Adapter/PirateBayAdapter.php
@@ -3,9 +3,14 @@ namespace Stien\Torrent\Adapter;
 
 use Stien\Torrent\HttpClientTrait;
 use Stien\Torrent\TorrentAdapterInterface;
+use Symfony\Component\DomCrawler\Crawler;
 
-class PirateBayAdapter implements TorrentAdapterInterface{
+class PirateBayAdapter implements TorrentAdapterInterface {
+
 	use HttpClientTrait;
+
+	private $baseUrl = "https://thepiratebay.se/";
+	private $searchUrl = "search/%QUERY%/0/7/0";
 
 	/**
 	 * @param array $options
@@ -23,6 +28,45 @@ class PirateBayAdapter implements TorrentAdapterInterface{
 	 */
 	public function search($query)
 	{
+		$url = $this->makeUrl($query);
+		$response = $this->httpClient->get($url);
 
+		$crawler = new Crawler((string)$response->getBody());
+		$items = $crawler->filter('#searchResult tr');
+
+		$results = [];
+		$first = true;
+		foreach ($items as $item)
+		{
+			// Ignore the first row, the header
+			if ( $first )
+			{
+				$first = false;
+				continue;
+			}
+			$result = new Torrent();
+			$itemCrawler = new Crawler($item);
+			$result->setName(trim($itemCrawler->filter('.detName')->text()));
+			$result->setSeeders((int)$itemCrawler->filter('td')->eq(2)->text());
+			$result->setLeechers((int)$itemCrawler->filter('td')->eq(3)->text());
+			$result->setMagnetUrl($itemCrawler->filterXpath('//tr/td/a')->attr('href'));
+			$results[] = $result;
+		}
+
+		return $results;
+	}
+
+	private function makeUrl($query, $category = null, $sort_by = null)
+	{
+		// TODO: Make URL based on category and sort.
+
+		// Make URL.
+		$url = $this->baseUrl . $this->searchUrl;
+
+		// Replace placeholder with actual query.
+		$query = urlencode($query);
+		$url = preg_replace("/%QUERY%/", $query, $url);
+
+		return $url;
 	}
 }
