3a42df15fc54b85c9896a2f844c6ac38e6c989cd
Bastian Stien <bastian.stien@gmail.com>
2015-03-02 23:23:31 +0100

    Updated ServiceProvider

--- src/TorrentServiceProvider.php
+++ src/TorrentServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 namespace Stien\Torrent;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
+use Stien\Torrent\Adapter\PirateBayAdapter;
 
 class TorrentServiceProvider extends ServiceProvider {
 
@@ -23,7 +25,14 @@ class TorrentServiceProvider extends ServiceProvider {
 	{
 		$this->app->bind('bstien.torrent.scraper', function ($app)
 		{
+			$torrentScraper = new TorrentScraper();
 
+			// Add PirateBayAdapter
+			$pirateBayAdapter = new PirateBayAdapter();
+			$pirateBayAdapter->setHttpClient(new \GuzzleHttp\Client());
+			$torrentScraper->addAdapter($pirateBayAdapter);
+
+			return $torrentScraper;
 		});
 	}
 
