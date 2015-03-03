aed07b5b164dc96c3aaa0b04ace4fcd3d6438f16
Bastian Stien <bastian.stien@gmail.com>
2015-03-03 11:17:37 +0100

    Surround $httpClient->get() within a try/catch-block. Piratebay seems to fail all the time

--- src/Adapter/PirateBayAdapter.php
+++ src/Adapter/PirateBayAdapter.php
@@ -1,7 +1,6 @@
 <?php
 namespace Stien\Torrent\Adapter;
 
-use GuzzleHttp\Client;
 use Stien\Torrent\HttpClientTrait;
 use Stien\Torrent\Result\Torrent;
 use Stien\Torrent\TorrentAdapterInterface;
@@ -30,16 +29,28 @@ class PirateBayAdapter implements TorrentAdapterInterface {
 	 */
 	public function search($query)
 	{
-		# Set single-cell view for results.
+		# Set single-cell view for torrents.
 		$requestOptions = [
+			'headers' => [
+				'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+			],
 			'cookies' => [
 				'lw' => 's'
 			]
 		];
-		$url = $this->makeUrl($query);
-		$response = $this->httpClient->get($url, $requestOptions);
 
-		$crawler = new Crawler((string)$response->getBody());
+
+		try
+		{
+			$url = $this->makeUrl($query);
+			$response = $this->httpClient->get($url, $requestOptions);
+			$crawler = new Crawler((string)$response->getBody());
+		} catch (\Exception $e)
+		{
+			// TODO: Log error. Some error has occured.
+			return [];
+		}
+
 		$items = $crawler->filter('#searchResult tr');
 
 		$torrents = [];
@@ -54,20 +65,20 @@ class PirateBayAdapter implements TorrentAdapterInterface {
 			}
 
 
-			$result = new Torrent();
+			$torrent = new Torrent();
 			$itemCrawler = new Crawler($item);
 
 			// Set details for torrent.
-			$result->setSite("PirateBay");
-			$result->setTitle(trim($itemCrawler->filter('td')->eq(1)->text()));
-			$result->setSeeders((int)$itemCrawler->filter('td')->eq(5)->text());
-			$result->setLeechers((int)$itemCrawler->filter('td')->eq(6)->text());
-			$result->setMagnet($itemCrawler->filterXpath('/td[3]/a[0]')->attr('href'));
-			$result->setSize($itemCrawler->filter('td')->eq(4)->text());
-			$result->setAge($itemCrawler->filterXPath('/td[2]')->text());
-			$result->setCategory($itemCrawler->filterXPath('/td[0]')->text());
+			$torrent->setSite("PirateBay");
+			$torrent->setTitle(trim($itemCrawler->filter('td')->eq(1)->text()));
+			$torrent->setSeeders((int)$itemCrawler->filter('td')->eq(5)->text());
+			$torrent->setLeechers((int)$itemCrawler->filter('td')->eq(6)->text());
+			$torrent->setMagnet($itemCrawler->filterXpath('/td[3]/a[0]')->attr('href'));
+			$torrent->setSize($itemCrawler->filter('td')->eq(4)->text());
+			$torrent->setAge($itemCrawler->filterXPath('/td[2]')->text());
+			$torrent->setCategory($itemCrawler->filterXPath('/td[0]')->text());
 
-			$torrents[] = $result;
+			$torrents[] = $torrent;
 		}
 
 		return $torrents;
@@ -75,8 +86,7 @@ class PirateBayAdapter implements TorrentAdapterInterface {
 
 	private function makeUrl($query, $category = null, $sort_by = null)
 	{
-		// TODO: Make URL based on category and sort.
-
+		// TODO: Make URL based on category and sort by fields.
 		// Make URL.
 		$url = $this->baseUrl . $this->searchUrl;
 
