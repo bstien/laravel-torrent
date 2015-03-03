95ee8ee804bcba8c1a46ec6ab5e94388e354cab5
Bastian Stien <bastian.stien@gmail.com>
2015-03-03 11:17:51 +0100

    Sort results in TorrentScraper

--- src/TorrentScraper.php
+++ src/TorrentScraper.php
@@ -16,13 +16,18 @@ class TorrentScraper {
 		{
 			$results[] = $adapter->search($query);
 		}
-
+		$results = array_flatten($results);
 		return $this->sortResults($results);
 	}
 
 	protected function sortResults($results)
 	{
 		// TODO: Do actual sorting.
+		// For now, sort it by seeders.
+		usort($results, function($a, $b){
+			return $a->getSeeders() <= $b->getSeeders();
+		});
+
 		return $results;
 	}
 }
